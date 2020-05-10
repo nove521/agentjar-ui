4e6a3b825f05b8fe6d61d8830d3f001cb368fd00
oldman <597800451@qq.com>
2020-05-10 20:03:50 +0800

    init

--- agentjar/src/main/java/com/cx/agent/LoadedAgent.java
+++ agentjar/src/main/java/com/cx/agent/LoadedAgent.java
@@ -1,12 +1,17 @@
-package com.cx;
+package com.cx.agent;
 
-import java.io.*;
+import org.yx.main.SumkServer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.lang.instrument.ClassDefinition;
 import java.lang.instrument.Instrumentation;
 import java.lang.instrument.UnmodifiableClassException;
+import java.util.Collections;
 
 public class LoadedAgent {
-    public static void agentmain(String agentArgs, Instrumentation inst) throws IOException, UnmodifiableClassException, ClassNotFoundException {
+    public static void agentmain(String agentArgs, Instrumentation inst) {
         System.out.println("Agent Main called");
         System.out.println("agentArgs : " + agentArgs);
         Class<?>[] allClass = inst.getAllLoadedClasses();
@@ -15,14 +20,31 @@ public class LoadedAgent {
             if (c.getSimpleName().endsWith("HelloController")){
 
                 ClassLoader classLoader = c.getClassLoader();
-                byte []b = readFileToByte(agentArgs);
+                byte []b = new byte[0];
+                try {
+                    b = readFileToByte(agentArgs);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 System.out.println(" 加载的类的大小：" + b.length);
                 if (b.length > 0){
                     ClassDefinition classDefinition = new ClassDefinition(c,b);
-                    inst.redefineClasses(classDefinition);
+                    try {
+                        inst.redefineClasses(classDefinition);
+                    } catch (ClassNotFoundException | UnmodifiableClassException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }
+
+        try {
+            SumkServer.start(Collections.singletonList("nohttp"));
+        }catch (Error e){
+            e.printStackTrace();
+        }
+
+        System.out.println("退出 agentjar");
     }
 
     private static byte[] readFileToByte(String path) throws IOException {
@@ -31,5 +53,9 @@ public class LoadedAgent {
         fileInputStream.read(b);
         fileInputStream.close();
         return b;
+    }
+
+    public static void main(String[] args) {
+        SumkServer.start();
     }
 }
